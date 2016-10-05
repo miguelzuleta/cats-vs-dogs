b6713e78ba7eb27126787d694eeb707d04db4fec
Miguel Zuleta <mzuleta@apple.com>
2016-10-05 13:48:45 -0700

    Working Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
-'use strict';
-
 var gulp = require('gulp');
 var haml = require('gulp-haml');
 var sass = require('gulp-sass');
 
-gulp.task('one', function(){
-	return gulp.src('uncompiled/haml/index.haml')
+gulp.task('haml', function(){
+	gulp.src('site/uncompiled/haml/index.haml')
 		.pipe(haml())
-		.pipe(gulp.dest('compiled'));
+		.pipe(gulp.dest('site/compiled'));
 });
 
 gulp.task('sass', function(){
-	return gulp.src('uncompiled/sass/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('compiled'))
+	gulp.src('site/uncompiled/sass/overview.scss')
+		.pipe(sass({
+			outputStyle: 'expanded',
+			sourceComments: true
+		}).on('error', sass.logError))
+		.pipe(gulp.dest('site/compiled'))
 });
 
-gulp.task('default', ['one', 'sass']);
+gulp.task('watch', function(){
+	gulp.watch('site/uncompiled/sass/*.scss', ['sass'])
+	gulp.watch('site/uncompiled/haml/*.haml', ['haml'])
+});
+
+gulp.task('default', ['haml', 'sass', 'watch']);
